b6be4692e3b54da0f015f4000e6e9fa86c4f7096
maximdocenko <maxdocenko23@gmail.com>
2020-07-17 15:21:16 +0500

    Seeds / Products table added

---
+++ database/seeds/ProductSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Hash;
+
+class ProductSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        for($i=1; $i<=15; $i++) {
+            DB::table('products')->insert([
+                'name' => Str::random(10),
+                'price' => rand(100, 1000),
+                'user_id' => rand(1,5),
+            ]);
+        }
+    }
+}
---
+++ database/seeds/UserSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        for($i=1; $i<=5; $i++) {
+            DB::table('users')->insert([
+                'name' => Str::random(10),
+                'email' => Str::random(10).'@gmail.com',
+                'phone' => Str::random(10),
+                'password' => Hash::make('password'),
+            ]);
+        }
+    }
+}
